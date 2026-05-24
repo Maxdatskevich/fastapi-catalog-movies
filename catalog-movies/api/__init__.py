67550824ce1ce7cac3fa4011a05456ea4da8d2d1
Maxdatskevich <155636509+Maxdatskevich@users.noreply.github.com>
2026-05-24 23:07:46 +0300

    Update api/__init__

--- catalog-movies/api/__init__.py
+++ catalog-movies/api/__init__.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import (
+    APIRouter,
+    Request,
+)
 from .api_v1 import router as api_v1_router
 
 # МОДУЛЬ ДЛЯ ХРАНЕНИЯ ВСЕЙ БИЗНЕС ЛОГИКИ
@@ -6,5 +9,16 @@ from .api_v1 import router as api_v1_router
 router = APIRouter(
     prefix="/api",
 )
+
+
+@router.get("/")
+def api_init(
+    request: Request,
+):
+    return {
+        "message": f"Hello! this is my first API",
+        "docs": "main API",
+    }
+
 
 router.include_router(api_v1_router)
--- catalog-movies/api/api_v1/__init__.py
+++ catalog-movies/api/api_v1/__init__.py
@@ -1,6 +1,19 @@
-from fastapi import APIRouter
+from fastapi import (
+    APIRouter,
+    Request,
+)
 
 # первая версия API
 router = APIRouter(
     prefix="/v1",
 )
+
+
+@router.get("/")
+def api_v1_init(
+    request: Request,
+):
+    return {
+        "message": f"Hello! this is API_v1",
+        "docs": "API_v1",
+    }
