cc88a14d9e34bb3ba2b99e51a9abbf6e955b5b6b
Maxdatskevich <155636509+Maxdatskevich@users.noreply.github.com>
2026-05-24 11:47:55 +0300

    Add new package: schemas

---
+++ catalog-movies/schemas/movie.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+
+# классы, которые описывают структуру данных
+# Pydantic для автоматической генерации документации Swagger и OpenAPI.
+class Movie(BaseModel):
+    id: int  # уникальный номер
+    title_f: str  # название
+    description_f: str  # описание
+    year_f: int | None  # год выпуска
+    director_f: str | None  # режиссер
+    budget_f: int | None  # бюджет
