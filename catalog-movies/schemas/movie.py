6d6ff51ae0f0d6581e9ff6a01cc39b0332d9e901
Maxdatskevich <155636509+Maxdatskevich@users.noreply.github.com>
2026-05-25 23:37:58 +0300

    Update Schemas

--- catalog-movies/schemas/movie.py
+++ catalog-movies/schemas/movie.py
@@ -7,6 +7,6 @@ class Movie(BaseModel):
     id: int  # уникальный номер
     title_f: str  # название
     description_f: str  # описание
-    year_f: int | None  # год выпуска
-    director_f: str | None  # режиссер
-    budget_f: int | None  # бюджет
+    year_f: int | None = None  # год выпуска
+    director_f: str | None = None  # режиссер
+    budget_f: int | None = None  # бюджет
