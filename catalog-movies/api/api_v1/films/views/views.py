a7ef8949112b7b18de7d46929bcf6fcdf67b6fac
Maxdatskevich <155636509+Maxdatskevich@users.noreply.github.com>
2026-05-31 22:07:57 +0300

    Add packege views, files views.py,details_views.py

---
+++ catalog-movies/api/api_v1/films/views/views.py
@@ -0,0 +1,44 @@
+from starlette.requests import Request
+
+from api.api_v1.films.crud import storage_movie
+from schemas.movie import (
+    Movie,
+    MovieAdd,
+)
+from fastapi import (
+    APIRouter,
+    status,
+)
+
+# endpoints (контроллеры).
+# МОДУЛЬ С ЗАВИСИМОСТЯМИ. ОБРАБАТЫВАЕМ http ЗАПРОСЫ, ОТДАЕМ ОТВЕТ ИЗ crud
+
+router = APIRouter(
+    prefix="/movies",
+    tags=["Movies"],
+)
+
+
+@router.get("/")
+def get_films(
+    request: Request,
+) -> dict:
+    return {
+        "message": f"Hello! this is API for films",
+        "docs": "main films",
+    }
+
+
+@router.get("/all_movies")
+def get_all_movies(
+    request: Request,
+) -> list:
+    return storage_movie
+
+
+@router.post("/add_new_film", response_model=Movie, status_code=status.HTTP_201_CREATED)
+def add_new_film(
+    new_film: MovieAdd,
+) -> Movie:
+
+    return Movie(**new_film.model_dump())
